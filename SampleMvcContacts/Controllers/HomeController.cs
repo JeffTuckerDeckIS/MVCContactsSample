b3ffc8d4df67363d8ee58a9d72b0ce7aacbbe387
Jeff Tucker <jeff.tucker@deckcommerce.com>
2015-03-12 10:55:30 -0500

    initial commit

---
+++ SampleMvcContacts/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMvcContacts.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<string> names = new List<string>();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+
+                OleDbCommand cmd = new OleDbCommand
+                {
+                    Connection = conn,
+                    CommandText = "select * from People",
+                    CommandType = CommandType.Text
+                };
+
+                OleDbDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    names.Add(dr["FirstName"].ToString());
+                }
+                dr.Close();
+
+            }
+
+
+            return View(names);
+        }
+    }
+}
