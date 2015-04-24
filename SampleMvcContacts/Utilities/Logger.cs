6605dc4d51f94aeb837cd65b9a057d8db032ab5a
Takashi Fujimoto <fujimoto.takashi@gmail.com>
2015-04-24 10:45:26 -0500

    solution

---
+++ SampleMvcContacts/Utilities/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SampleMvcContacts.Utilities
+{
+    public static class Logger
+    {
+        public static void Log(string errorMessage)
+        {
+            try
+            {
+                string logFile = MakeDateFile(@"c:\temp\SampleMVCContactLog-yyyy.MM.dd.txt");
+
+                System.IO.StreamWriter w = System.IO.File.AppendText(logFile);
+                w.WriteLine(errorMessage);
+                w.Close();
+            }
+            catch
+            { 
+                // Please allow ASP.NET, IISUSER, or EVERYONE access to C:\temp directory.
+            }           
+
+        }
+
+        /// <summary>
+        /// Help FileName to add Date and Time
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns>File Name with Date Time info</returns>
+        private static string MakeDateFile(string filepath)
+        {
+            filepath = filepath.Replace("yyyy", System.DateTime.Now.ToString("yyyy"));
+            filepath = filepath.Replace("MM", System.DateTime.Now.ToString("MM"));
+            filepath = filepath.Replace("mm", System.DateTime.Now.ToString("MM"));
+            filepath = filepath.Replace("dd", System.DateTime.Now.ToString("dd"));
+            return filepath;
+        }
+    }
+}
